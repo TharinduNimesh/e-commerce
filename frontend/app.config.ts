b725e3f50ceb44d5855a149caf4f7f7a15d76d76
TharinduNimesh <127307898+TharinduNimesh@users.noreply.github.com>
2024-02-01 23:55:31 +0530

    Account Edit And Delete Page on Settings

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -12,5 +12,25 @@ export default defineAppConfig({
       base: "uppercase text-xs font-bold",
       font: "",
     },
+    input: {
+      default: {
+        size: "lg",
+      },
+      color: {
+        gray: {
+          outline: "dark:bg-[#2b2a33]",
+        },
+      },
+    },
+    select: {
+      default: {
+        size: "lg",
+      },
+      color: {
+        gray: {
+          outline: "dark:bg-[#2b2a33]",
+        },
+      },
+    },
   },
 });
