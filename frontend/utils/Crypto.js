69ade04c8ff80d000abbc07d5f13e8373205a979
TharinduNimesh <127307898+TharinduNimesh@users.noreply.github.com>
2024-02-15 19:42:08 +0530

    Update Crypto.js

--- frontend/utils/Crypto.js
+++ frontend/utils/Crypto.js
@@ -1,6 +1,6 @@
 import CryptoJS from 'crypto-js';
 
-const PRIVATE_KEY = 'f538612bf21ec0064fb91f36fc9fa8a65a9321a19cc1721a14b0402b6a8de62de7a7e1d7d4252dc03ac0749d80284facd0b67357c8acead8efceb531be3fd64e';
+const PRIVATE_KEY = import.meta.env.VITE_PW_PRIVATE_KEY;
 
 export const Crypto = {
     encrypt: (text) => {
